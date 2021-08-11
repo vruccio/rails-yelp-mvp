4f5798b127fef349b2b558c6057e9c94a7462695
violette ruccio <violetteruccio@gmail.com>
2021-08-11 16:47:37 +0200

    done

--- Gemfile
+++ Gemfile
@@ -47,3 +47,7 @@ gem 'rspec-rails', group: [ :test ]
 gem 'rails-controller-testing', group: [ :test ]
 gem 'rspec-rails', group: [ :test ]
 gem 'rails-controller-testing', group: [ :test ]
+
+gem 'autoprefixer-rails'
+gem 'font-awesome-sass', '~> 5.6.1'
+gem 'simple_form'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+pure = { name: 'Pure', address: 'Cormontaigne', category:'french', phone_number: '0320020405' }
+clarance = { name: 'Clarance', address: 'Vieux Lille', category:'french', phone_number: '0320020405' }
+bellezza = { name: 'Bellezza', address: 'Rue Royale', category:'italian', phone_number: '0320020405' }
+jane = { name: 'Jane', address: 'Rue Faidherbe', category:'japanese', phone_number: '0320020405' }
+weara = { name: 'Weara', address: 'Grand Place', category:'belgian', phone_number: '0320020405' }
+
+[pure, clarance, bellezza, jane, weara].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
